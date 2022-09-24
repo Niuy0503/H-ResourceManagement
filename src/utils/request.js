d2611398a8e874824139cca6ac2a00576d710e6f
xxy <1791459798@qq.com>
2022-09-24 10:51:55 +0800

    feat:port和修改标题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,20 @@
 import axios from 'axios'
 
+// 通过axios创建axios实例
 const service = axios.create({
-  baseURL: '/dev', // 基准地址
+  // 环境变量的作用
+  // 1.正常公司中有几个环境
+  // 开发dev
+  // 测试test
+  // 预发uat
+  // 线上pro
+
+  // 2.在项目里如何配置这几个环境
+  // 通过.env 配置base api
+  // 开发环境的接口前缀/api
+  // 线上环境的接口前缀/prod-api
+
+  baseURL: process.env.VUE_APP_BASE_API, // 基准地址
   timeout: 5000
 })
 
